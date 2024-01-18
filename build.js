062f19498216cc37bfe60594450db7c24f854b51
Kristofer <kristofer@fmckl.se>
2024-01-18 15:49:47 +0100

    Update build configuration in build.js

--- build.js
+++ build.js
@@ -3,15 +3,9 @@ import esbuild from "esbuild";
 esbuild.build({
   entryPoints: ["./src/index.tsx"],
   bundle: true,
-  outfile: "dist/index.jsx",
+  outdir: "dist",
   format: "esm",
-  minify: true,
-  external: [
-    "react",
-    "react-dom",
-    "@dfinity/agent",
-    "@dfinity/identity",
-    "@dfinity/candid",
-  ],
+  splitting: true,
+  external: ["react", "react-dom"],
   plugins: [],
 });
